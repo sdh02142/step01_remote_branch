07f3649447f37ebda99b65da6974573e967865c6
sdh02142 <sdh02142@naver.com>
2026-08-19 16:26:10 +0900

    modify select() method from OrderController.java at main due to making rebase

--- src/mvc/controller/OrderController.java
+++ src/mvc/controller/OrderController.java
@@ -6,6 +6,7 @@ public class OrderController {
 		System.out.println("edit from cart branch");
 		System.out.println("edit from feature/test branch");
 		System.out.println("edit from main");
+		System.out.println("edit for retry about rebase test from main");
 	}
 
 }
