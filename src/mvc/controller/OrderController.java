afe4764e8c57e0458dc1190a68d5b4c36df8b0bd
sdh02142 <sdh02142@naver.com>
2026-08-19 16:23:02 +0900

    re_test about rebase from test branch

--- src/mvc/controller/OrderController.java
+++ src/mvc/controller/OrderController.java
@@ -5,6 +5,7 @@ public class OrderController {
 	public void select() {
 		System.out.println("edit from cart branch");
 		System.out.println("edit from main");
+		System.out.println("edit from test branch");
 	}
 
 }
